866c7490ea52ff39bd934ec843b4676a5983e310
Yurelle Gamier <yurelle.gamier@gmail.com>
2023-04-25 23:45:09 -0400

    Implement the Security checks (and associated Unit Tests) specified in Section 6 of the RFC-9285 standard.

--- src/main/java/io/yurelle/Base45.java
+++ src/main/java/io/yurelle/Base45.java
@@ -4,7 +4,9 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
+
+import static io.yurelle.SecureLookupWrapper.doLookup;
+import static io.yurelle.SecureLookupWrapper.doReverseLookup;
 
 /**
  * Java implementation of the Base45 Standard outlined here:
@@ -14,41 +16,28 @@ import java.nio.charset.StandardCharsets;
  * provided in the spec, along with several others.
  */
 public class Base45 {
-    public final static char[] ALPHANUM_STANDARD = new char[] {
-            // 0 - 9
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-
-            // A - Z
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-            'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-            'U', 'V', 'W', 'X', 'Y', 'Z',
-
-            // Symbols
-            ' ', '$', '%', '*', '+', '-', '.', '/', ':'
-    };
-    public final static int[] ALPHANUM_REVERSE_LOOKUP = new int[] {
-            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  // 0x00 - 0x0f
-            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  // 0x10 - 0x1f
-            36, -1, -1, -1, 37, 38, -1, -1, -1, -1, 39, 40, -1, 41, 42, 43,  // 0x20 - 0x2f
-            0,   1,  2,  3,  4,  5,  6,  7,  8,  9, 44, -1, -1, -1, -1, -1,  // 0x30 - 0x3f
-            -1, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,  // 0x40 - 0x4f
-            25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, -1, -1, -1, -1, -1,  // 0x50 - 0x5f
-    };
-
     public static String encode(final byte[] inputData) throws IOException {
         return encode(new ByteArrayInputStream(inputData));
     }
 
+    //
+    //Variable names match those used in the encoding examples provided in the standard.
+    //See: Section 4(.0)
+    //
     public static String encode(final InputStream in) throws IOException {
         final StringBuilder strOut = new StringBuilder();
+
+        int bytesRead = 0;
         while(in.available() > 0) {
             //First Byte
             int a = in.read();
+            bytesRead++;
 
             //Odd-Even Check
             if (in.available() > 0) {//Two Source Bytes
                 //Second Byte
                 int b = in.read();
+                bytesRead++;
 
                 //Unified Bytes
                 int n = (a * 256) + b;
@@ -62,23 +51,23 @@ public class Base45 {
                 int c = e_remainder % 45;
 
                 //Output
-                strOut.append(ALPHANUM_STANDARD[c]);
-                strOut.append(ALPHANUM_STANDARD[d]);
-                strOut.append(ALPHANUM_STANDARD[e]);
+                strOut.append(doLookup(c, bytesRead));
+                strOut.append(doLookup(d, bytesRead));
+                strOut.append(doLookup(e, bytesRead));
 
             } else {//Only One Source Byte
                 int c = a % 45;
                 int d = a / 45;
 
                 //Output
-                strOut.append(ALPHANUM_STANDARD[c]);
-                strOut.append(ALPHANUM_STANDARD[d]);
+                strOut.append(doLookup(c, bytesRead));
+                strOut.append(doLookup(d, bytesRead));
             }
         }
         return strOut.toString();
     }
 
-    public static byte[] decode(final String inputStr) throws IOException {
+    public static byte[] decode(final String inputStr) throws IOException, IllegalArgumentException {
         //Prep for InputStream
         //
         //The Encoding Specified in the Standard; See: Section 4, Paragraph 2
@@ -88,35 +77,52 @@ public class Base45 {
         return decode(new ByteArrayInputStream(buf));
     }
 
-    public static byte[] decode(final InputStream in) throws IOException {
+    //
+    //Variable names match those used in the decoding examples provided in the standard.
+    //See: Section 4(.0)
+    //
+    public static byte[] decode(final InputStream in) throws IOException, IllegalArgumentException {
         final ByteArrayOutputStream out = new ByteArrayOutputStream();
 
+        int bytesRead = 0;
         while(in.available() > 0) {
             //First byte
             //
             //Read in next char and translate back through lookup table
-            int c = ALPHANUM_REVERSE_LOOKUP[in.read()];
+            int c = doReverseLookup(in.read(), bytesRead);
+            bytesRead++;
 
             //Check for expected value
             //
             //There must be a minimum of 2 bytes, if not, then it's an error.
+            //See: Section 4, Paragraphs 6 & 7
             if (in.available() < 0) {
-                throw new IllegalArgumentException("Unexpected end of input! Parser expected at least one more byte.");
+                throw new IllegalArgumentException("Unexpected end of input at index '" + bytesRead + "'! Parser expected at least one more byte.");
             }
 
             //Second byte
-            int d = ALPHANUM_REVERSE_LOOKUP[in.read()];
+            int d = doReverseLookup(in.read(), bytesRead);
+            bytesRead++;
 
             //Check for 3rd byte
             boolean hasThirdByte = in.available() > 0;
             int e = 0;
             if (hasThirdByte) {//3rd Byte Exists
                 //Third byte
-                e = ALPHANUM_REVERSE_LOOKUP[in.read()];
+                e = doReverseLookup(in.read(), bytesRead);
+                bytesRead++;
             }
 
             //Combine Bytes
             int accumulator = c + (d * 45) + (e * 45 * 45);
+
+            //Security check outlined in the standard; See: Section 6, Paragraph 5.
+            //
+            //Base45 triplets exceeding an accumulated value of 64K (i.e. 65,535; or 0xFFFF)
+            //must be rejected.
+            if (accumulator > 0xFFFF) {
+                throw new IllegalArgumentException("Parsing Failed! Parsed value '" + accumulator + "' of Base45 chunk at index '" + (hasThirdByte ? bytesRead-3 : bytesRead-2) + "' exceeds double-byte max value of 64K (i.e. 65,535; or 0xFFFF).");
+            }
 
             //b
             byte b = (byte) accumulator;
